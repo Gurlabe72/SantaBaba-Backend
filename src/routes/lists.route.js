0a0aa10f9d7e8b90c8e946e989a46d1dd349ef9b
Gurlabe Dhaliwal <GurlabeD@Ramans-MacBook-Air.local>
2018-12-28 17:22:16 -0800

    working on lists route for both the Naughty and the Nice List :

--- src/routes/lists.route.js
+++ src/routes/lists.route.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router();
-const ctrl = require('../controllers/posts.controller')
+const ctrl = require('../controllers/lists.controller')
 //Your Routes folder is where Express and Express-Router come to play.
 //You have to add ctrl.________<--- and you put your module.exports here for every CRUD 
 //The Routes for a user is written like so, below.
 
-router.get('/', ctrl.getAllPosts)
+router.get('/', ctrl.getAllLists)
+router.get('/:id', ctrl.getOneList),
+    router.delete('/:id', ctrl.deleteList),
+    router.post('/', ctrl.createList)
 
+module.exports = router 
